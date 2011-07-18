155aed85bd1c3226bc7ab4c27a57f28692e8e3e0
Ondřej Beneš <ondra.benes@gmail.com>
2011-07-18 16:11:15 +0200

    Small change for Honza

--- src/net/skimap/SkimapServlet.java
+++ src/net/skimap/SkimapServlet.java
@@ -5,9 +5,10 @@ import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class SkimapServlet extends HttpServlet {
+	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.getWriter().println("Hello, Honza");
 	}
 }
